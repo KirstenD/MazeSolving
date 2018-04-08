f64d8ad7f388872ec60deb709b8c89eed490e041
KirstenD <kdawes@Lap293.utk.tennessee.edu>
2018-04-08 17:35:25 -0400

    changed name to initalized

---
+++ intialMaze.py
@@ -0,0 +1,45 @@
+import generateMaze as gm
+#import node 
+import sys
+import random 
+def startPoint(maze):
+    randX = random.randint(1,len(maze[0])-1)
+    randY = random.randint(1,len(maze)-1)
+    while(maze[randY][randX] =='True'):
+        randX = random.randint(1,len(maze[0])-1)
+        randY = random.randint(1,len(maze)-1)
+    return (randX,randY)
+
+def endPoint(maze,startPoint):
+    randX = random.randint(1,len(maze[0])-1)
+    randY = random.randint(1,len(maze)-1)
+    while((maze[randY][randX] =='True') and (startPoint ==(randX,randY))):
+        randX = random.randint(1,len(maze[0])-1)
+        randY = random.randint(1,len(maze)-1)
+
+    return (randX,randY)
+def mazeChange(maze,col, row):
+    maze2= [] 
+    for i in range(0, len(maze)):
+        mazeX = []
+        for j in  range(0, len(maze[0])):
+                mazeX.append(str(maze[i][j]))
+        maze2.append(mazeX)
+    return maze2
+
+def startEnd(maze):
+    x = mazeChange(maze, cols, rows) 
+    start = startPoint( x)
+    end = endPoint( x, start)
+    #x[start[1]][start[0]] = "S"
+    #x[end[1]][end[0]] = "E"
+    return (start,end)
+def main():
+    rows = int(sys.argv[1])    
+    cols = int(sys.argv[2])    
+    mazeCreate = gm.maze(cols,rows)
+    x = mazeChange(mazeCreate, cols, rows) 
+    intial= startEnd(x)
+    start = intial[0]
+    end = intial[1]
+main()
